4b124100959265db55d91d4ad26350065627ab4e
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-19 14:00:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(array)
-array.each do |array|
-  array.reverse
+array.reverse do |array|
+  puts ""
   end
 
 def reverse_each_word(sentence2)
-  array.each do |array|
-    array.reverse
+  array.reverse do |array|
+    puts ""
 end
 
 def reverse_each_word(array)
