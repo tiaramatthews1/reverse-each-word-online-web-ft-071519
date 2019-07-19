c29aa681d35ccc68be10d54c2af742bfc5582653
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-19 14:09:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,5 @@
-def reverse_each_word(array)
-array.reverse do |array|
-  puts "olleH ,ereht dna woh era ?uoy"
-  end
 
 def reverse_each_word(array)
   array.collect do |array|
-  puts 
   end
 end
