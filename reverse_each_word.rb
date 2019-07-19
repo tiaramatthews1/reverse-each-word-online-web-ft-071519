745e148fcd4140f15b343741500c6a2055e9df0d
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-19 13:58:18 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(array)
+array.each do |array|
+  end
+
+def reverse_each_word(sentence2)
+  
+end
+
+def reverse_each_word(array)
+  array.collect do |array|
+  puts 
+  end
+end
